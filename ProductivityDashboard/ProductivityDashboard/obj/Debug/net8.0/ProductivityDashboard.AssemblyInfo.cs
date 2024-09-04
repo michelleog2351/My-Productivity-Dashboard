66fa3d6d2364301b9ce4d2c3068b4ff3a9fa4e55
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-09-04 22:28:24 +0100

    Add input box for user to enter their name for personalised feel - partially complete

--- ProductivityDashboard/ProductivityDashboard/obj/Debug/net8.0/ProductivityDashboard.AssemblyInfo.cs
+++ ProductivityDashboard/ProductivityDashboard/obj/Debug/net8.0/ProductivityDashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductivityDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6385a42ab0421ab458600eea78474e0bfe895704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a642bd716d022db9c9b60695b3b734194adfea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductivityDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductivityDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
