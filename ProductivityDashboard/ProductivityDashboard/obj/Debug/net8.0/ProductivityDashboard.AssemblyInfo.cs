6ad540f30c1d5f8f7a4ed3deef0c947fc0986628
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-09-12 18:54:35 +0100

    Modify Start and Reset Buttons to be round buttons, add timer label to the top of the timer

--- ProductivityDashboard/ProductivityDashboard/obj/Debug/net8.0/ProductivityDashboard.AssemblyInfo.cs
+++ ProductivityDashboard/ProductivityDashboard/obj/Debug/net8.0/ProductivityDashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductivityDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4d9bd21a1c5d93d6895ca07b48289dd305aaa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d17d4187299e28448526e35080c7f099b5c33f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductivityDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductivityDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
